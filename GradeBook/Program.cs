c01297ec87a333ae41eaa6419d696747c6626a30
devannea <devannenotes@gmail.com>
2019-05-25 02:28:21 -0500

    Updated tictactoe

---
+++ GradeBook/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the GradeBook!");
+            Start();
+            Console.Read();
+        }
+
+        static void Start()
+        {
+            Dictionary<string, string> myDict = new Dictionary<string, string>();
+            while (true)
+            {
+                Console.WriteLine("Please enter the name of the student, or say 'Quit' to evaluate the grades of the student(s).");
+                string studentName = Console.ReadLine();
+                if (studentName != "quit")
+                {
+                    Console.WriteLine("Please enter the grades of the student. Round to the nearest integer. Do not use decimals.");
+                    Console.WriteLine("Separate the grades using a space. Do not use commas or any other punctuation.");
+                    Console.WriteLine("Example: 91 84 79 100");
+                    string studentGrades = Console.ReadLine();
+                    myDict.Add(studentName, studentGrades);
+                }
+                if (studentName == "quit")
+                {
+                    string studentName;
+                    string studentGrades;
+                    string[] gradeArray;
+                    int[] iGrades;
+                    foreach (var i in studentGrades.Keys)
+                    {
+                        studentName = i;
+                        studentGrades = studentGrades[i];
+                        gradeArray = studentGrades.Split(' ');
+                        iGrades = Array.ConvertAll(gradeArray, int.Parse);
+                    }
+                }
+            }
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,88 +9,139 @@ namespace TicTacToe
     class Program
     {
 
-        static string CurrentPlayer = " ";
-        const int BoardSize = 3;
-        static string[,] board = new string[BoardSize, BoardSize] { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } }; // initialize board
-        static int TotalMoves = 0;
-        static bool GameOver = false;
+        static string[] squares = new string[10] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        static string currentPlayer = "O";
+        static int moves = 0;
 
         static void Main(string[] args)
         {
-            ResetBoard();
-            StartGame();
+            while(Win() == false && Tie() == false)
+            {
+                Console.Clear();
+                printBoard();
+                changeMarker();
+                Console.WriteLine("Player " + currentPlayer + ", please select a square.");
+                string input = Console.ReadLine();
+                placeMark(input);
+            }
+            Console.Clear();
+            printBoard();
+            if(Win() == true && Tie() == false)
+            {
+                Console.WriteLine(currentPlayer + " wins!");
+            } else if (Win() == false && Tie() == true)
+            {
+                Console.WriteLine("It's a tie.");
+            }
+            
             Console.Read();
         }
 
-        public static void ResetBoard()
+        // Print the game board
+        static void printBoard()
         {
-            PrintBoard();
+            Console.WriteLine("{0} | {1} | {2}", squares[1], squares[2], squares[3]);
+            Console.WriteLine("---------");
+            Console.WriteLine("{0} | {1} | {2}", squares[4], squares[5], squares[6]);
+            Console.WriteLine("---------");
+            Console.WriteLine("{0} | {1} | {2}", squares[7], squares[8], squares[9]);
         }
 
-        public static void StartGame()
+        // Place X or O
+        static void placeMark(string input)
         {
-            PlaceMark();
+            int XorO = Convert.ToInt32(input);
+            squares[XorO] = currentPlayer;
+            Win();
         }
 
-        #region Print the game board
-        static void PrintBoard()
+        // Marker switches between X and O
+        static void changeMarker()
         {
-
-            for (int j = 0; j < BoardSize; j++)
+            if (currentPlayer == "X")
             {
-                Console.WriteLine("-------"); // row guidelines
-                for (int i = 0; i < BoardSize; i++)
-                {
-                    Console.Write("|" + board[i, j].ToString()); // column guidelines, write on one line, not writeline on separate lines
-                }
-                Console.WriteLine("|"); // closing board
-            }
-            Console.WriteLine("-------"); // closing board
-            Console.Read();
-        }
-        #endregion
-
-        static void PlaceMark(int RowPos, int ColPos)
-        {
-            if (RowPos < 1 || RowPos > BoardSize || ColPos < 1 || ColPos > BoardSize)
-            {
-                Console.WriteLine($"Invalid row position or column position. Please check your input and try again.");
-                return;
-            }
-            int RowIdx = RowPos - 1;
-            int ColIdx = ColPos - 1;
-            if (board[RowIdx, ColIdx] == " ")
-            {
-                board[RowIdx, ColIdx] = CurrentPlayer;
+                currentPlayer = "O";
             }
             else
             {
-                Console.WriteLine("That spot has already been marked. Please choose another spot.");
-                StartGame();
+                currentPlayer = "X";
             }
-            TotalMoves++;
-            //if (HasWon())
-            //{
-                //GameOver = true;
-            //}
-            //else if (IsTie())
-            //{
-                //GameOver = true;
-            //}
-            //else
-            //{
-                //if (CurrentPlayer == "X")
-                //{
-                    //CurrentPlayer = "O";
-                //}
-                //else
-                //{
-                    //CurrentPlayer = "X";
-                //}
-            //}
-            //PrintBoard();
-
+            moves++;
         }
 
+        // If you get three in a row
+        static bool RowWin()
+        {
+            if (squares[1] == squares[2] && squares[2] == squares[3])
+            {
+                return true;
+            } else if (squares[4] == squares[5] && squares[5] == squares[6])
+            {
+                return true;
+            } else if (squares[7] == squares[8] && squares[8] == squares[9])
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        
+        // If you get three in a column
+        static bool ColumnWin()
+        {
+            if (squares[1] == squares[4] && squares[4] == squares[7])
+            {
+                return true;
+            } else if (squares[2] == squares[5] && squares[5] == squares[8])
+            {
+                return true;
+            } else if (squares[3] == squares[6] && squares[6] == squares[9])
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        // If you get three diagnolly
+        static bool DiagonalWin()
+        {
+            if (squares[1] == squares[5] && squares[5] == squares[9])
+            {
+                return true;
+            } else if (squares[3] == squares[5] && squares[5] == squares[7])
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        // Determine win
+        static bool Win()
+        {
+            if (RowWin() == false && ColumnWin() == false && DiagonalWin() == false)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        // Determine tie
+        static bool Tie()
+        {
+            if (Win() == false && moves == 9)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
     }
 }
