c811219312c26f79580fa9e9940cec12ffda952f
devannea <devannenotes@gmail.com>
2019-05-28 11:24:03 -0500

    Updated TowerHanoi, unfinished, not optimized

--- TowerHanoi/Program.cs
+++ TowerHanoi/Program.cs
@@ -11,8 +11,21 @@ namespace TowerHanoi
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Towers of Hanoi!");
+            Start();
             PrintBoard();
             Console.Read();
+        }
+
+        static void Start()
+        {
+            PrintBoard();
+            Console.WriteLine("Choose the disc you'd like to move first.");
+            int discChoice = int.Parse(Console.ReadLine());
+            if (discChoice != 1 && discChoice != 2 && discChoice != 3 && discChoice != 4)
+            {
+                Console.WriteLine("You have entered an incorrect response. Please check your input and try again.");
+            }
+            Console.WriteLine("You have selected " + discChoice);
         }
 
         static void PrintBoard()
