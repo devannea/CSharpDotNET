8f659b359a862cdc2cd8cac69a37f92145fff3e7
devannea <devannenotes@gmail.com>
2019-05-28 11:15:06 -0500

    Initial set up for TowerHanoi

---
+++ TowerHanoi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Towers of Hanoi!");
+            PrintBoard();
+            Console.Read();
+        }
+
+        static void PrintBoard()
+        {
+            Console.WriteLine("A: {4} {3} {2} {1}");
+            Console.WriteLine("B: { }");
+            Console.WriteLine("C: { }");
+        }
+
+    }
+}
