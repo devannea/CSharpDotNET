65c7caaf169703f98ec66db59511683597a810d4
devannea <devannenotes@gmail.com>
2019-06-15 19:37:46 -0500

    Completed for the second time Maybe not the last

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -18,7 +18,7 @@ namespace GradeBook
         static void Start()
         {
             Dictionary<string, string> gradebook = new Dictionary<string, string>();
-            while (true)
+            while (true) // Start of loop
             {
                 Console.WriteLine("Please enter the name of the student, or say 'Done' to evaluate the grades of the student(s).");
                 string nameInput = Console.ReadLine();
@@ -26,25 +26,26 @@ namespace GradeBook
                 {
                     Console.WriteLine("Please enter the grades of the student. Round to the nearest whole number integer. Do not use decimals.");
                     Console.WriteLine("Separate the grades using a space. Do not use commas or any other punctuation.");
-                    Console.WriteLine("Example: 91 84 79 100");
+                    Console.WriteLine("");
                     string gradeInput = Console.ReadLine();
                     gradebook.Add(nameInput, gradeInput);
-                    Console.WriteLine("Please enter the name of the student, or say 'Done' to evaluate the grades of the student(s).");
-                    nameInput = Console.ReadLine();
                 }
-                
-                foreach (var key in gradebook.Keys)
+                else
                 {
-                    int[] gradeArray = Array.ConvertAll<string, int>(gradebook[key].Split(), Convert.ToInt32);
-                    double gradeAverage = gradeArray.Average();
-                    int gradeLowest = gradeArray.Min();
-                    int gradeHighest = gradeArray.Max();
-                    Console.WriteLine(" ");
-                    Console.WriteLine("Student: " + key);
-                    Console.WriteLine("Grade Average: " + Math.Round(gradeAverage, 2, MidpointRounding.AwayFromZero));
-                    Console.WriteLine("Lowest Grade: " + gradeLowest);
-                    Console.WriteLine("Highest Grade: " + gradeHighest);
+                    break;
                 }
+            } // End of loop
+            foreach (var key in gradebook.Keys)
+            {
+                int[] gradeArray = Array.ConvertAll<string, int>(gradebook[key].Split(), Convert.ToInt32);
+                double gradeAverage = gradeArray.Average(); // LINQ
+                int gradeLowest = gradeArray.Min(); // LINQ
+                int gradeHighest = gradeArray.Max(); // LINQ
+                Console.WriteLine(" ");
+                Console.WriteLine("Student: " + key);
+                Console.WriteLine("Grade Average: " + Math.Round(gradeAverage, 2, MidpointRounding.AwayFromZero));
+                Console.WriteLine("Lowest Grade: " + gradeLowest);
+                Console.WriteLine("Highest Grade: " + gradeHighest);
             }
         }
     }
